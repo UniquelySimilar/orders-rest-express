a72a261516de3bfd9d8279c6c1a51f176984c11e
UniquelySimilar <consciousexplorer@gmail.com>
2020-09-18 10:00:31 -0600

    deleted client files unnecessary for REST

--- routes/customers.js
+++ routes/customers.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET customers listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('customer index');
 });
 
 module.exports = router;
