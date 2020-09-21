ed8d2638550f2667cd10d8dcd136d6650c9c90c1
UniquelySimilar <consciousexplorer@gmail.com>
2020-09-21 14:27:29 -0600

    customer crud

--- controllers/customer-controller.js
+++ controllers/customer-controller.js
@@ -1,8 +1,9 @@
+import mysql from 'mysql';
 import pool from '../mysql-conn-pool.js';
 
 class CustomerController {
   findAll(callback) {
-    pool.query('SELECT * FROM customers', function(error, results, fields) {
+    pool.query('SELECT * FROM customers', (error, results, fields) => {
       if (error) throw error;
 
       return callback(results);
@@ -10,7 +11,7 @@ class CustomerController {
   }
 
   find(id, callback) {
-    pool.query('SELECT * FROM customers WHERE id = ?', [id], function(error, results, fields) {
+    pool.query('SELECT * FROM customers WHERE id = ?', [id], (error, results, fields) => {
       if (error) throw error;
 
       // Convert result from array of RowDataPacket objects to array of object literals
@@ -21,6 +22,39 @@ class CustomerController {
       }
 
       return callback(customer);
+    });
+  }
+
+  create(customer, callback) {
+    pool.query('INSERT INTO customers SET ?', customer, (error, results, fields) => {
+      if (error) throw error;
+
+      // Return OkPacket object
+      return callback(results);
+    });
+  }
+
+  update(id, customer, callback) {
+    let updatedAt = mysql.raw('CURRENT_TIMESTAMP()');
+    let sql = 'UPDATE customers SET first_name = ?, last_name = ?, city = ?, state = ?, zipcode = ?, ' +
+              'home_phone = ?, work_phone = ?, email = ?, updated_at = ? WHERE id = ?';
+    let queryValues = [
+      customer.first_name, customer.last_name, customer.city, customer.state, customer.zipcode,
+      customer.home_phone, customer.work_phone, customer.email, updatedAt, id
+    ];
+
+    pool.query(sql, queryValues, (error, results, fields) => {
+      if (error) throw error;
+
+      return callback(results);
+    });
+  }
+
+  delete(id, callback) {
+    pool.query('DELETE FROM customers WHERE id = ?', [id], (error, results, fields) => {
+      if (error) throw error;
+
+      return callback(results);
     });
   }
 }
--- routes/customer-routes.js
+++ routes/customer-routes.js
@@ -5,16 +5,44 @@ import CustomerController from '../controllers/customer-controller.js'
 var customerController = new CustomerController();
 
 /* GET customers listing. */
-router.get('/', function(req, res, next) {
-  customerController.findAll(function(results) {
+router.get('/', (req, res) => {
+  customerController.findAll( results => {
+    res.send(results);
+  });
+});
+
+router.get('/:customerId', (req, res) => {
+  let id = req.params.customerId;
+  customerController.find(id, results => {
     res.send(results);
   })
 });
 
-router.get('/:customerId', function(req, res, next) {
-  let id = req.params.customerId;
-  customerController.find(id, function(results) {
-    res.send(results);
+router.post('/', (req, res) => {
+  customerController.create(req.body, (results) => {
+    res.sendStatus(201);
+  });
+});
+
+router.put('/:id', (req, res) => {
+  customerController.update(req.params.id, req.body, (results) => {
+    if (results.affectedRows === 0) {
+      res.sendStatus(404);
+    }
+    else {
+      res.sendStatus(204);
+    }
+  });
+});
+
+router.delete('/:id', (req, res)=> {
+  customerController.delete(req.params.id, (results) => {
+    if (results.affectedRows === 0) {
+      res.sendStatus(404);
+    }
+    else {
+      res.sendStatus(204);
+    }
   })
 })
 
